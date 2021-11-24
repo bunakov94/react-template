387e9d4e23983b6041664e249a2c82831a7b882e
bunakov94 <bunakov.code@gmail.com>
2021-11-24 20:19:32 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const App = (): JSX.Element => {
+  return <Main>App</Main>
+}
+
+const Main = styled.main`
+  color: black;
+  font-size: 20px;
+`
